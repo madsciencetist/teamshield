6c7d754e5b86d507872a68e25a143f8bb848e541
Andrew Reiter <reiterandrew@gmail.com>
2016-06-25 16:15:13 -0700

    Added GetMeasurement to estimator skeleton

--- scripts/estimator.py
+++ scripts/estimator.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Point
+from teamshield.srv import GetMeasurement
 
-def current_location_callback(current_location):
-    rospy.loginfo("I am at %f %f %f", current_location.x, current_location.y, current_location.z);
-    
-def estimator():
+class Estimator:
+  def current_location_callback(self, current_location):
+      rospy.loginfo("I am at %f %f %f", current_location.x, current_location.y, current_location.z);
 
-    rospy.init_node('estimator')
+      
+      in_new_cell = True # logic required here
+      if in_new_cell:
+        try:
+          response = self.get_measurement(current_location.x, current_location.y, current_location.z);
+          measurement = response.measurement
+          rospy.loginfo("Measured %f", measurement);
+        except rospy.ServiceException, e:
+          rospy.loginfo("Service call failed: %s", e);
+      
+  def __init__(self):
 
-    rospy.Subscriber("current_location", Point, current_location_callback)
+      rospy.init_node('estimator')
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+      rospy.Subscriber("current_location", Point, self.current_location_callback)
+      
+      self.get_measurement = rospy.ServiceProxy('get_measurement', GetMeasurement)
 
 if __name__ == '__main__':
-    estimator()
+    Estimator()
+    rospy.spin()
